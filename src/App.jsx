68f1fa58f4247aec84c4bccac1e0be3af5cb024f
Arnab-dot <technofries1209@gmail.com>
2026-01-14 02:23:43 +0530

    fix of the api calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
+// Force deployment update
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import LandingPage from './pages/LandingPage';
